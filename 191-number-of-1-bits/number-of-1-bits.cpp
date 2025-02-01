ab9a50ee4de8483b2af186dd6da3e3f9c1684445
LikithaKodidela <160643442+LikithaKodidela@users.noreply.github.com>
2025-02-01 22:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (12.19%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-           return __builtin_popcount(n);
+           //return __builtin_popcount(n);
+             int count = 0;
+        while (n) {
+            count += (n & 1);  // Check if the least significant bit is 1
+            n >>= 1;            // Right shift n to process the next bit
+        }
+        return count;
+
     }
 };
