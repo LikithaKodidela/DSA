e44577dca7c4fc985b711dab91b43950382ffe02
LikithaKodidela <160643442+LikithaKodidela@users.noreply.github.com>
2025-02-09 10:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (84.66%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -5,8 +5,8 @@ class Solution {
         
         while(n>0){
             if(n==1)return true;
-            if(n %2 !=0)break;
-            n /=2;
+            if(n %2 !=0)break; // when ever the input is something that is divisible by 2 but not power of 2 
+            n /=2;            // or the input is not divisible by 2 the loop will break and returns false
         }
         return false;
     }
