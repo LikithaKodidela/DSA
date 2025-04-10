a4120099cfc3b9b2bfbc147e4ecfc056f9deeb94
LikithaKodidela <160643442+LikithaKodidela@users.noreply.github.com>
2025-04-10 22:23:30 +0530

    Time: 547 ms (75.27%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -3,3 +3,11 @@ select  player_id,min(event_date)
 as first_login from Activity 
 group by player_id;
 
+/*with activity_cte as(
+        select player_id, event_date,
+        row_number() over (partition by player_id order by event_date) as rn
+        from Activity
+)
+select player_id, event_date as first_login 
+from activity_cte
+where rn = 1*/
