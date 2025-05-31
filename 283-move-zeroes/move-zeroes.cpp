63b61195ca0c9d071519b2e4ec886f1c8c085d5f
LikithaKodidela <160643442+LikithaKodidela@users.noreply.github.com>
2025-06-01 01:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (78.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int nonzeroindex=0;
-       for(int i=0;i<nums.size();i++)
-       {
-         if(nums[i]!=0)
-          {
-             nums[nonzeroindex++]=nums[i];
-          }
-       } 
-       for(int i=nonzeroindex;i<nums.size();i++)
-       {
-          nums[i]=0;
-       }
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+              nums[j++]=nums[i];
+            }
+        }
+        for(int i=j;i<nums.size();i++)
+        {
+            nums[i]=0;
+        }
     }
 };
