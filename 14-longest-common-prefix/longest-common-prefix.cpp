eab4c6526c014d7ea7d6906296789a426b20cac3
LikithaKodidela <160643442+LikithaKodidela@users.noreply.github.com>
2025-06-08 22:28:24 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (49.63%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,7 +5,7 @@ public:
         {
             return "";
         }
-       /* string prefix=strs[0];
+        string prefix=strs[0];
         for(int i=1;i<strs.size();i++)
         {
             int j=0;
@@ -19,22 +19,6 @@ public:
                 return "";
             }
         }
-        return prefix;
-    }*/
-    string prefix = "";
-
-        for (int i = 0; i < strs[0].length(); i++) {
-            char currentChar = strs[0][i];
-            
-            for (int j = 1; j < strs.size(); j++) {
-                if (i >= strs[j].length() || strs[j][i] != currentChar) {
-                    return prefix;
-                }
-            }
-
-            prefix += currentChar;
-        }
-
         return prefix;
     }
 };
