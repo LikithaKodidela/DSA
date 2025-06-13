e23183c9863982386f3cd96ad32c0478cd56f2d7
LikithaKodidela <160643442+LikithaKodidela@users.noreply.github.com>
2025-06-13 22:27:02 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (58.58%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size() / 2];
-        
+          map<int,int> mpp;
+        int freq=0,res=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            mpp[nums[i]]++;
+        }
+        for(auto &it:mpp)
+        {
+            if(it.second>freq)
+            {
+                res=it.first;
+                freq=it.second;
+            }
+        }
+        return res;
+    
     }
 };
