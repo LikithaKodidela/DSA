ea9d1c64df41e2bad40770819c9768985961aa7f
LikithaKodidela <160643442+LikithaKodidela@users.noreply.github.com>
2025-06-21 21:37:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.95%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int p1=m-1;
         int p2=n-1;
         int p=m+n-1;
-        while(p1>=0 && p2>=0)
+        while(p1>=0&&p2>=0)
         {
             if(nums1[p1]>nums2[p2])
             {
@@ -18,7 +18,7 @@ public:
             }
             p--;
         }
-        while(p2 >=0)
+        while(p2>=0)
         {
             nums1[p]=nums2[p2];
             p2--;
