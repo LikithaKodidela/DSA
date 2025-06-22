4818055407cc6d6522db86c8b2629e2c85c84b9d
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2025-06-22 22:16:48 +0530

    Time: 79 ms (31.88%) | Memory: 53.9 MB (5.35%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,7 +3,7 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         int cnt=0,presum=0;
-        unordered_map<int,int> mpp;
+        map<int,int> mpp;
         mpp[0]=1;
         for(int i=0;i<n;i++)
         {
