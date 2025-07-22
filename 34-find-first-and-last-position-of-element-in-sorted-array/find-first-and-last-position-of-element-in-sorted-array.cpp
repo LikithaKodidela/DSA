30efc1fe37cab143e360c4ff56b79e3d0a082c49
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2025-07-22 22:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.51%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = lowerBound(nums,target);
-        int last = upperBound(nums, target);
+        int first =firstOccurrence(nums,target);
         if(first==nums.size()||nums[first]!=target)return {-1,-1};
-        return {first,last-1};
+         int last =lastOccurrence(nums,target);
+        return {first,last};
     }
 
-      int lowerBound(vector<int> &nums,int target)
+      int firstOccurrence(vector<int> &nums,int target)
       {
         int begin=0,n=nums.size();
         int last=n-1;
@@ -15,18 +15,21 @@ public:
         while(begin<=last)
         {
            int mid=begin+(last-begin)/2;
-           if(nums[mid]>=target)
+           if(nums[mid]==target)
            {
                ans=mid;
                last=mid-1;
            }
-           else{
+           else if(nums[mid]<target){
             begin=mid+1;
+           }
+           else{
+            last=mid-1;
            }
         }
         return ans;
       } 
-      int upperBound(vector<int>nums,int target)
+      int lastOccurrence(vector<int>nums,int target)
       {
         int begin=0,n=nums.size();
         int last=n-1;
@@ -34,13 +37,16 @@ public:
         while(begin<=last)
         {
            int mid=begin+(last-begin)/2;
-           if(nums[mid]>target)
+           if(nums[mid]==target)
            {
                ans=mid;
-               last=mid-1;
+               begin=mid+1;
+           }
+            else if(nums[mid]<target){
+            begin=mid+1;
            }
            else{
-            begin=mid+1;
+            last=mid-1;
            }
         }
         return ans;
