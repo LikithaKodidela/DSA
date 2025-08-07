d0cec3d2093e508700397ef1725c8e6a07be851c
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2025-08-07 05:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (37.96%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,26 +1,7 @@
 class Solution {
 public:
-   void Shift(string &s)
-   {
-      int n=s.length();
-      char ele=s[0];
-      for(int i=1;i<n;i++)
-      {
-        s[i-1]=s[i];
-      }
-      s[n-1]=ele;
-      
-   }
+ 
     bool rotateString(string s, string goal) {
-        int n=goal.length();
-        for(int i=0;i<n;i++)
-        {
-            Shift(s);
-            if(s==goal)
-            {
-                return true;
-            }
-        }
-        return false;
+          return s.size()==goal.size() && (s+s).find(goal)!=-1;
     }
 };
