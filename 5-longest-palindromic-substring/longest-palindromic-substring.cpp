53968561b0d77a5d3ed358a18542ef0103856a61
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2025-08-26 22:33:46 +0530

    Time: 7 ms (88.26%) | Memory: 9.4 MB (81.96%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,31 +1,38 @@
 class Solution {
 private:
-   string expand_from_center(string s,int left, int right) {
-            while (left >= 0 && right < s.length() && s[left] == s[right]) {
-                left--;
-                right++;
-            }
-            return s.substr(left + 1, right - left - 1);
+    // returns a pair: (start_index, length)
+    pair<int,int> expand_from_center(const string &s, int left, int right) {
+        while (left >= 0 && right < (int)s.length() && s[left] == s[right]) {
+            left--;
+            right++;
         }
+        // left and right are now one step beyond the palindrome
+        return make_pair(left + 1, right - left - 1);
+    }
+
 public:
     string longestPalindrome(string s) {
         if (s.length() <= 1) {
             return s;
         }
-        string max_str = s.substr(0, 1);
 
-        for (int i = 0; i < s.length() - 1; i++) {
-            std::string odd = expand_from_center(s,i, i);
-            std::string even = expand_from_center(s,i, i + 1);
+        int best_start = 0;
+        int best_len = 1;  // at least one char
 
-            if (odd.length() > max_str.length()) {
-                max_str = odd;
+        for (int i = 0; i < (int)s.length() - 1; i++) {
+            pair<int,int> odd  = expand_from_center(s, i, i);
+            pair<int,int> even = expand_from_center(s, i, i + 1);
+
+            if (odd.second > best_len) {
+                best_start = odd.first;
+                best_len   = odd.second;
             }
-            if (even.length() > max_str.length()) {
-                max_str = even;
+            if (even.second > best_len) {
+                best_start = even.first;
+                best_len   = even.second;
             }
         }
 
-        return max_str;
+        return s.substr(best_start, best_len);
     }
 };
