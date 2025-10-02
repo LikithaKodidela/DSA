50889dbaadcf1a36618cf4f96b0576217d5cc8d1
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2025-10-03 00:11:10 +0530

    Time: 43 ms (45.41%) | Memory: 75.7 MB (20.67%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -9,24 +9,60 @@
  * };
  */
 class Solution {
+private:
+    ListNode* findMiddle(ListNode* head)
+    {
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* merge2list(ListNode* head1,ListNode* head2)
+    {
+        ListNode* t1=head1;
+        ListNode* t2=head2;
+        ListNode* dNode=new ListNode(-1);
+        ListNode* temp=dNode;
+        while(t1!=NULL && t2!=NULL)
+        {
+            if(t1->val < t2->val)
+            {
+                temp->next=t1;
+                temp=t1;
+                t1=t1->next;
+            }
+            else{
+                temp->next=t2;
+                temp=t2;
+                t2=t2->next;
+            }
+        }
+        if(t1)
+        {
+            temp->next=t1;
+        }
+        else
+        {
+            temp->next=t2;
+        }
+        return dNode->next;
+    }
 public:
     ListNode* sortList(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp=head;
-        int i=0;
-        while(temp!=NULL)
-        {
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
-        sort(arr.begin(),arr.end());
-        temp=head;
-        while(temp!=NULL)
-        {
-           temp->val=arr[i];
-           temp=temp->next;
-           i=i+1;
-        }
-        return head;
+       if(head==NULL || head->next==NULL)
+       {
+           return head;
+       }
+       ListNode* middle=findMiddle(head);
+       ListNode* lefthead=head;
+       ListNode* righthead=middle->next;
+       middle->next=NULL;
+       lefthead=sortList(lefthead);
+       righthead=sortList(righthead);
+       return merge2list(lefthead,righthead);
     }
 };
