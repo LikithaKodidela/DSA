4de0bec1a156984cd91cfe19b3d6ce69b944ee80
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2025-10-04 05:49:28 +0530

    Time: 36 ms (65.76%) | Memory: 18.5 MB (82.46%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -4,27 +4,46 @@
  *     int val;
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ * }; 
  */
 class Solution {
+    ListNode* collisionPoint(ListNode* t1,ListNode* t2,int d)
+    {
+        while(d)
+        {
+            d--;
+            t2=t2->next; 
+        }
+        while(t1!=t2)
+        {
+            t1=t1->next;
+            t2=t2->next;
+        }
+        return t1;
+    }
 public:
     ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
-        unordered_map<ListNode*,int> mpp;
-        ListNode* temp=head1;
-        while(temp!=NULL)
-        {
-            mpp[temp]=1;
-            temp=temp->next;
-        }
-        temp=head2;
-        while(temp!=NULL)
-        {
-            if(mpp.find(temp)!=mpp.end())
-            {
-                return temp;
-            }
-            temp=temp->next;
-        }
-        return NULL;
+      int n1=0;
+      int n2=0;
+      ListNode* t1=head1;
+      while(t1!=NULL)
+      {
+        n1++;
+        t1=t1->next;
+      }
+      ListNode* t2=head2;
+      while(t2!=NULL)
+      {
+        n2++;
+        t2=t2->next;
+      }
+      if(n1 < n2)
+      {
+         return collisionPoint(head1,head2,n2-n1);
+      }
+      else
+      {
+        return collisionPoint(head2,head1,n1-n2);
+      }
     }
 };
