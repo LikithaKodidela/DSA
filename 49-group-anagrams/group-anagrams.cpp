6dfad8394fa50fadfbd68aa0afee38285f3d787f
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2025-12-07 06:17:56 +0530

    Time: 12 ms (83.46%) | Memory: 24.8 MB (85.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,42 +1,19 @@
 class Solution {
-    bool checkChars(string &str1,string &str2)
-    {
-       if(str1.size()!=str2.size()) return false;
-
-       vector<int> freq(26,0);
-
-       for(char c:str1) freq[c-'a']++;
-       for(char c:str2) freq[c-'a']--;
-
-       for(int x:freq)
-       {
-         if(x!=0) return false;
-       }
-       return true;
-
-    }
+    
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n=strs.size();
-        vector<vector<string>> ans;
-        vector<bool> used(n,false);
+        unordered_map<string,vector<string>> mp;
 
-        for(int i=0;i<n;i++)
+        for(string s:strs)
         {
-           if(used[i]) continue;
-           vector<string> group;
-           group.push_back(strs[i]);
-           used[i]=true;
-           for(int j=i+1;j<n;j++)
-           {
-             if(!used[j]&& checkChars(strs[i],strs[j]))
-             {
-                group.push_back(strs[j]);
-                used[j]=true;
-             }
-           }
-           ans.push_back(group);
+            string key=s;
+            sort(key.begin(),key.end());
+            mp[key].push_back(s);
         }
+
+        vector<vector<string>> ans;
+        for (auto &p:mp) ans.push_back(p.second);
+
         return ans;
     }
 };
