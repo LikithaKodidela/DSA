9fc00af18a4ec0c63a75bbcbf0bdfea8ea577303
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2025-12-26 04:29:00 +0530

    Time: 2 ms (59.03%) | Memory: 14.2 MB (54.51%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,30 +1,27 @@
 class Solution {
-public:
-    vector<vector<int>> ans;
+void solve(int i, vector<int>& arr, vector<int>& temp, int target,vector<vector<int> > &ans) {
+       if(target==0)
+       {
+         ans.push_back(temp);
+         return;
+       }
+       if(target<0 || i==arr.size())
+       {
+         return;
+       }
 
-    void solve(int i, vector<int>& arr, vector<int>& temp, int target) {
-        if (target == 0) {
-            ans.push_back(temp);
-            return;
-        }
-
-        if (target < 0)
-            return;
-
-        if (i == arr.size())
-            return;
-
-        solve(i + 1, arr, temp, target);
-
-        temp.push_back(arr[i]);
-        solve(i, arr, temp, target - arr[i]);
-        temp.pop_back();
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+       temp.push_back(arr[i]);
+       solve(i,arr,temp,target-arr[i],ans);
        
+       temp.pop_back();
+       solve(i+1,arr,temp,target,ans);
+      
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) { 
         vector<int> temp;
-        solve(0, arr, temp, target);
+        vector<vector<int> > ans;
+        solve(0, arr, temp, target,ans);
         return ans;
     }
 };
