0c68d9ce00680844adc3fc44b87b29f5ed63dc3f
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2026-01-02 23:15:39 +0530

    Time: 0 ms (100.00%) | Memory: 29.9 MB (31.07%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,17 +3,17 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-       /* for(int j=0;j<k;j++)
-        {
-            int last=nums[n-1];
-            for(int i=n-1;i>0;i--)
-            {
-                nums[i]=nums[i-1];
-            }
-            nums[0]=last;
-        }*/
+        if(k==0) return;
+        vector<int> temp(nums.begin()+(n-k),nums.end());
+        
+       
+       for(int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+        
+        for(int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+       
     }
 };
