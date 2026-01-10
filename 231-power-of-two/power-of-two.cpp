63f45ac0f87436667319e3c982e6c4ce3d90f6ee
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2026-01-10 05:33:37 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (13.87%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,13 +1,6 @@
 class Solution {
-    public:
+public:
     bool isPowerOfTwo(int n) {
-        if(n==0)return false;
-        
-        while(n>0){
-            if(n==1)return true;
-            if(n %2 !=0)break; // when ever the input is something that is divisible by 2 but not power of 2 
-            n /=2;            // or the input is not divisible by 2 the loop will break and returns false
-        }
-        return false;
+         return (n>0 && (n&(n-1))==0);
     }
 };
