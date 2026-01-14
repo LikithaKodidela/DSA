37fa8ddcb862288d54c08ec21264880e7220694a
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2026-01-14 23:34:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (79.72%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.length();
+        unordered_set<string> dict(wordDict.begin(),wordDict.end());
+        vector<bool> dp(n+1,false);
+        dp[0]=true;
+        int maxLen=0;
+        for(const string& word: wordDict){
+            maxLen=max(maxLen,(int)word.size());
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=max(0,i-maxLen);j<i;j++){
+                if(dp[j] && dict.find(s.substr(j,i-j))!=dict.end())
+                {
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
