2ac94f97766250fce4f2bb2c092be46da53bbbe7
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2026-01-24 22:39:33 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (25.45%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
+        int xorResult=start^goal;
         int count=0;
-        while(goal>0 || start >0)
+        while(xorResult)
         {
-            if((start&1)!=(goal&1))
-            {
-                count++;
-            }
-            start>>=1;
-            goal>>=1;
+            count+=xorResult&1;
+            xorResult>>=1;
         }
         return count;
     }
