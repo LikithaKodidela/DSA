016687c048e13b840eb3d673c91da0db92b78a07
Likitha chowdary <160643442+LikithaKodidela@users.noreply.github.com>
2026-01-28 22:05:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (87.25%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    bool isValid(string s) 
-    {
-         stack<char> st;
-        // unordered_map<char,char> mapping={{')','('} , {'}','{'} , {']','['}}
-         for(char c:s)
-         {
-            if(c=='('||c=='{'||c=='[')
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch : s)
+        {
+            if(ch=='(' || ch=='[' || ch=='{')
             {
-                st.push(c);
+                st.push(ch);
             }
             else
             {
-                if(st.empty())
-                {
-                    return false;
-                }
-                char top=st.top();
-                if((c==')'&&top=='(')||(c=='}'&& top=='{')||(c==']'&&top=='['))
+                if (st.empty()) return false;
+                if((ch==')'&& st.top()=='(')||
+                   (ch==']'&& st.top()=='[')||
+                    (ch=='}'&&st.top()=='{'))
                 {
                     st.pop();
                 }
-                else
-                {
+                else{
                     return false;
                 }
             }
-         }
-         return st.empty();
-        
-
+        }
+        return st.empty();
     }
 };
