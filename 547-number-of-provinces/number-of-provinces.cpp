4f98d0586115c05c35dd79ddcfc022ba7657b4c9
Likitha Kodidela <160643442+LikithaKodidela@users.noreply.github.com>
2026-05-26 22:10:19 +0530

    Time: 4 ms (25.30%) | Memory: 20.7 MB (29.48%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -11,6 +11,26 @@ public:
                }
           }
     }
+    void bfs(int start,vector<int> adj[],vector<int> &visited)
+    {
+        visited[start] = 1;
+        queue<int> q;
+        q.push(start);
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            for(auto it : adj[node])
+            {
+                if(!visited[it])
+                {
+                    visited[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
         vector<int> visited(n,0);
