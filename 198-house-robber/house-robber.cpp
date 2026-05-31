6bcdd1917e18cf8465aab55950ba9d943afdc462
Likitha Kodidela <160643442+LikithaKodidela@users.noreply.github.com>
2026-05-31 12:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (43.19%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int solve(int ind,int n,vector<int> &nums,vector<int> &dp)
+    {
+        if(ind >=n) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int notTake = solve(ind+1,n,nums,dp);
+        int take = nums[ind] + solve(ind+2,n,nums,dp);
+
+        return dp[ind]=max(take,notTake);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> dp(n,-1);
+        return solve(0,n,nums,dp);
+    }
+};
