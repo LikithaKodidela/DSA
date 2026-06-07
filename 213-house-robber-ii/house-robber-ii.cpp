9d99796943bc0a648c26e3b55a580628db91508d
Likitha Kodidela <160643442+LikithaKodidela@users.noreply.github.com>
2026-06-07 13:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (97.15%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,13 +1,12 @@
 class Solution {
-    int solve(vector<int>& nums)
+    int solve(vector<int>& nums,int start,int end)
     {
         int n=nums.size();
-        if(n == 1) return nums[0];
 
-        int prev = nums[0];
+        int prev = 0;
         int prev2 = 0;
        
-        for(int i=1;i<n;i++)
+        for(int i=start;i<=end;i++)
         {
            int take = nums[i];
            if(i>1) take+=prev2;
@@ -22,13 +21,6 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n == 1) return nums[0];
-        
-        vector<int> temp1,temp2;
-        for(int i=0;i<n;i++)
-        {
-            if(i!=0) temp1.push_back(nums[i]);
-            if(i!=n-1) temp2.push_back(nums[i]);
-        }
-        return max(solve(temp1),solve(temp2));
+        return max(solve(nums,0,n-2),solve(nums,1,n-1));
     }
 };
