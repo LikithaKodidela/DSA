f81157d67ea0e259a18fa6d41e4529dfd11971fd
Likitha Kodidela <160643442+LikithaKodidela@users.noreply.github.com>
2026-06-13 10:29:54 +0530

    Time: 128 ms (55.05%) | Memory: 13.3 MB (92.25%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,26 +1,28 @@
 class Solution {
-    bool solve(int target,vector<int>& nums,vector<vector<bool>> &dp)
+    bool solve(int target,vector<int>& nums)
     {
         int n = nums.size();
+        vector<bool> curr(target+1,0) , prev(target+1,0);
        for(int i =0;i<n;i++)
        {
-          dp[i][0] = true;
+          prev[0] = true;
        }
-       if ( nums[0]<=target) dp[0][nums[0]] = true;
+       if ( nums[0]<=target) prev[nums[0]] = true;
        for(int i=1;i<n;i++)
        {
          for(int k=1;k<=target;k++)
          {
-            bool notTake = dp[i-1][k];
+            bool notTake = prev[k];
             bool take = false ;
             if(nums[i]<=k) 
             {
-                take = dp[i-1][k-nums[i]];
+                take = prev[k-nums[i]];
             }
-            dp[i][k] = take || notTake;
+            curr[k] = take || notTake;
          }
+         prev = curr;
        }
-       return dp[n-1][target];
+       return prev[target];
     }
 
 public:
@@ -35,7 +37,7 @@ public:
          if(sum % 2 != 0) return false;
 
          int target = sum/2;
-        vector<vector<bool>> dp(n,vector<bool>(target+1,false));
-         return solve(target,nums,dp);
+     
+         return solve(target,nums);
     }
 };
