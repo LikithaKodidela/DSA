4953f2122af34536841be26103269ba03a4f9107
Likitha Kodidela <160643442+LikithaKodidela@users.noreply.github.com>
2026-06-20 06:43:36 +0530

    Time: 783 ms (16.17%) | Memory: 11.7 MB (84.24%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
     int solve(int ind, int currSum , vector<int>& nums , int target)
     {
-        if (ind == nums.size())
+        if (ind < 0 )
         {
             return currSum==target;
         }
         
-        int plus = solve(ind+1,currSum+nums[ind],nums,target);
-        int minus = solve(ind+1,currSum-nums[ind],nums,target);
+        int plus = solve(ind-1,currSum+nums[ind],nums,target);
+        int minus = solve(ind-1,currSum-nums[ind],nums,target);
         return plus +  minus;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(0,0,nums,target);
+        int n = nums.size();
+        return solve(n-1,0,nums,target);
     }
 };
