00c6d4448a43b297fb6b5e423008affc5493ac7d
Likitha Kodidela <160643442+LikithaKodidela@users.noreply.github.com>
2026-06-23 22:53:25 +0530

    Time: 698 ms (17.36%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
--- select tweet_id from Tweets where not char_length(content)<=15;
-select tweet_id from Tweets where  char_length(content)>15;
+SELECT tweet_id FROM Tweets 
+WHERE LENGTH(content) > 15;
