18a5baf3506098010b9d3a2196c9a91c258b76ce
Likitha Kodidela <160643442+LikithaKodidela@users.noreply.github.com>
2026-07-02 04:53:40 +0530

    Time: 967 ms (5.05%) | Memory: 11.6 MB (93.79%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,28 +1,17 @@
-
-
 class Solution {
-    int dp[21][2001];
-
-    int solve(int ind, int currSum, vector<int>& nums, int target)
+    int solve(int ind, int currSum , vector<int>& nums , int target)
     {
-        if (ind < 0)
-            return currSum == target;
-
-        // currSum ranges from -1000 to 1000
-        if (dp[ind][currSum + 1000] != -1)
-            return dp[ind][currSum + 1000];
-
-        int plus = solve(ind - 1, currSum + nums[ind], nums, target);
-        int minus = solve(ind - 1, currSum - nums[ind], nums, target);
-
-        return dp[ind][currSum + 1000] = plus + minus;
+        if (ind == nums.size())
+        {
+            return currSum==target;
+        }
+        
+        int plus = solve(ind+1,currSum+nums[ind],nums,target);
+        int minus = solve(ind+1,currSum-nums[ind],nums,target);
+        return plus +  minus;
     }
-
 public:
-    int findTargetSumWays(vector<int>& nums, int target)
-    {
-        memset(dp, -1, sizeof(dp));
-
-        return solve(nums.size() - 1, 0, nums, target);
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return solve(0,0,nums,target);
     }
 };
