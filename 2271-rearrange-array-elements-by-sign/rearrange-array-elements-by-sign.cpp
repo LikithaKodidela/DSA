80c3ce944c3a2d39cf1e2f67e693ab92a5987c44
Likitha Kodidela <160643442+LikithaKodidela@users.noreply.github.com>
2026-07-27 21:36:42 +0530

    Time: 14 ms (13.54%) | Memory: 138.9 MB (21.40%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n=nums.size();
-         vector<int> res(n);
-        int neg=1,pos=0;
+        int n = nums.size();
+        vector<int> ans(n),even,odd;
+        
         for(int i=0;i<n;i++)
         {
-            if(nums[i]>0)
+            if(nums[i]<0)
             {
-                res[pos]=nums[i];
-                pos+=2;
+                odd.push_back(nums[i]);
             }
-            else if(nums[i]<0)
+            else
             {
-                res[neg]=nums[i];
-                neg+=2;
+                even.push_back(nums[i]);
             }
         }
-        return res;
+        for(int i=0;i<n/2;i++)
+        {
+            ans[2*i+1] = odd[i];
+            ans[2*i] = even[i];
+        }
+        return ans;
     }
 };
